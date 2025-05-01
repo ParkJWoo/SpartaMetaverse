b8f78582eaae2afcba299c3ff0f3b7b7ac59c8e1
ParkJWoo <character453@naver.com>
2025-05-01 14:40:40 +0900

    feat: 장애물 생성 및 반복 생성 로직 적용 완료

---
+++ SpartaMetaverse/Assets/Scripts/2. FlappyPlaneScene/BgLooper.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgLooper : MonoBehaviour
+{
+    public int numBgCount = 5;
+
+    public int obstacleCount = 0;
+    public Vector3 obstacleLastPosition = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  GameObject.FindObjectsOfType: <> 안에 있는 타입의 여러 오브젝트들을 탐색하는 함수
+        Obstacle[] obstacles = GameObject.FindObjectsOfType<Obstacle>();
+
+        obstacleLastPosition = obstacles[0].transform.position;
+        obstacleCount = obstacles.Length;
+
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            obstacleLastPosition = obstacles[i].SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered: " + collision.name);
+
+        //  collision.CompareTag: 충돌한 오브젝트의 태그 명을 비교하는 함수
+        if (collision.CompareTag("Background"))
+        {
+            //  이 때, 충돌한 오브젝트를 잠시 BoxCollider2D로 형변환 시킨 후, 사이즈 값을 가져온다.
+            float widthOfBgObject = ((BoxCollider2D)collision).size.x;
+            Vector3 pos = collision.transform.position;
+
+            pos.x += widthOfBgObject * numBgCount;
+            collision.transform.position = pos;
+
+            //  이미 Background로 처리를 했다면, obstacle이 아니므로 더이상 동작하는 것을 방지하고자 return을 선언한다.
+            return;
+        }
+
+        Obstacle obstacle = collision.GetComponent<Obstacle>();
+
+        //  만약 충돌한 오브젝트가 장애물이라면
+        if (obstacle)
+        {
+            //  우리가 알고 있는 장애물이니, 부딪힌 후에 장애물의 위치를 다시 세팅한다.
+            obstacleLastPosition = obstacle.SetRandomPlace(obstacleLastPosition, obstacleCount);
+        }
+    }
+}
---
+++ SpartaMetaverse/Assets/Scripts/2. FlappyPlaneScene/Obstacle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float highPosY = 1.0f;
+    public float lowPosY = -1.0f;
+
+    //  위와 아래 장애물 사이의 간격
+    public float holeSizeMin = 1.0f;
+    public float holeSizeMax = 3.0f;
+
+    public Transform topObject;
+    public Transform bottomObject;
+
+    public float widthPadding = 4.0f;
+
+    public Vector2 SetRandomPlace(Vector2 lastPosition, int obstacleCount)
+    {
+        float holeSize = Random.Range(holeSizeMax, holeSizeMin);
+        float halfHoleSize = holeSize / 2;
+
+        topObject.localPosition = new Vector2(0, halfHoleSize);
+        bottomObject.localPosition = new Vector2(0, -halfHoleSize);
+
+        Vector2 placePosition = lastPosition + new Vector2(widthPadding, 0);
+        placePosition.y = Random.Range(lowPosY, highPosY);
+
+        transform.position = placePosition;
+
+        return placePosition;
+    }
+}
