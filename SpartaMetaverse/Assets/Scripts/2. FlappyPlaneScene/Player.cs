50f56b48e4277c7caada72fe106411b6126955ef
ParkJWoo <character453@naver.com>
2025-05-01 14:10:23 +0900

    feat: 플레이어 충돌 처리 기능 추가

--- SpartaMetaverse/Assets/Scripts/2. FlappyPlaneScene/Player.cs
+++ SpartaMetaverse/Assets/Scripts/2. FlappyPlaneScene/Player.cs
@@ -4,18 +4,106 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region 플레이어가 사용할 변수 선언
     Animator animator;
     Rigidbody2D _rigidbody2D;
+
+    public float flapForce = 6.0f;
+    public float forwardSpeed = 3.0f;
+    public bool isDead = false;
+    float deathCoolDown = 0.0f;
+
+    bool isFlap = false;
+    #endregion
+
+    public bool godMode = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if(animator == null)
+        {
+            Debug.LogError("Not Found Animator!!");
+
+        }
+
+        if(_rigidbody2D == null)
+        {
+            Debug.LogError("Not Found Rigidbody2D!!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //  죽었을 때와 죽지 않았을 때를 구분하여 동작
+        if(isDead)
+        {
+            if(deathCoolDown <= 0.0f)
+            {
+                //  게임 재시작
+            }
+
+            else
+            {
+                deathCoolDown -= Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                //  플레이어 점프
+                isFlap = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isDead)
+        {
+            return;
+        }
+
+        else
+        {
+            Vector2 velocity = _rigidbody2D.velocity;
+            velocity.x = forwardSpeed;
+
+            if(isFlap)
+            {
+                velocity.y += flapForce;
+                isFlap = false;
+            }
+
+            _rigidbody2D.velocity = velocity;
+
+            //  점프 시 플레이어 오브젝트를 회전시킴
+            float angle = Mathf.Clamp((_rigidbody2D.velocity.y * 10.0f), -90, 90);
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(godMode)
+        {
+            return;
+        }
+
+        if(isDead)
+        {
+            return;
+        }
+
+        isDead = true;
+        deathCoolDown = 1.0f;
+
+        animator.SetBool("IsDie", true);
     }
 }
